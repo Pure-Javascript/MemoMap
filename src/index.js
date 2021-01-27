f23d331489d06f6788d926283c2d02d9fe5055b9
davidyang2149 <davidyang2149@gmail.com>
2021-01-27 19:19:11 +0900

    Update index(Add Naver Map)
    Memo Map.
    - Update Naver Map.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import "./style.css"
 const main = document.getElementById('main');
 
 main.innerHTML = '<div class="textBlue">Hello World!</div>';
+
+window.onload = () => {
+  let map = null;
+
+  const initMap = () => {
+    map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10
+    });
+  }
+
+  initMap();
+};
