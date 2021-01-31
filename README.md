a50edf54dd7a7fc5c49238912e7ea756920a8405
arahansa <arahansa@naver.com>
2021-01-31 21:24:36 +0900

    주소 검색 추가 이후 CORS 문제로 인해 보류

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # MemoMap
 
 [Memo Map Homepage](https://pure-javascript.github.io/MemoMap)
+
+- 집 주변 검색해서 지도에 표시 기능
+-- 주소를 좌표로 변환하는 기능 필요
+- 해당 위치에 등록 하여 TODO 리스트처럼 리스트화 ( 별도 창)
+- 목록에서 삭제 가능
+- 저장된 목록 메일 전송..? 저장 후 다시 불러오기 기능?
+
+
+## 예제
+- https://navermaps.github.io/maps.js.ncp/docs/tutorial-digest.example.html
+
+
+## 참고 도움 주소
+- 네이버 콘솔 https://console.ncloud.com/mc/solution/naverService/application
+
+## 튜토리얼
+- https://apidocs.ncloud.com/ko/ai-naver/maps_geocoding/
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css"
+const axios = require('axios')
 
 const main = document.getElementById('main');
 
@@ -21,3 +22,21 @@ window.onload = () => {
 
   initMap();
 };
+
+
+document.getElementById("search").addEventListener("click", function(e){
+ console.log("버튼 클릭 ")
+ const searchValue = document.getElementById("search-input").value;
+ console.log("searchValu :", searchValue)
+
+ axios.get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query="+searchValue, {
+  timeout: 4000,
+  headers: {
+   'X-NCP-APIGW-API-KEY-ID': '생략',
+   'X-NCP-APIGW-API-KEY':'생략'
+  },
+  crossdomain: true
+ }).then(data =>{
+  console.log("data :", data);
+ });
+})
